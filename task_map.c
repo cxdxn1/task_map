4b8e40729c27d99adf68f8f38b507e071d20dff0
Rayyan Khan <145166752+cxdxn1@users.noreply.github.com>
2024-12-28 20:17:21 +0000

    use mach_vm_ API instead of vm_

--- task_map.c
+++ task_map.c
@@ -1,3 +1,4 @@
+#include <mach/mach_vm.h>
 #include <mach/mach.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -5,39 +6,41 @@
 
 mach_port_t gTask = 0;
 
-vm_address_t task_map() {
+mach_vm_address_t task_map() {
     gTask = mach_task_self();
-    vm_address_t addr = 0;  
+    mach_vm_address_t addr = 0;  
 
     // Map virtual memory
-    vm_map(gTask, &addr, PAGE_SIZE, 0, VM_FLAGS_ANYWHERE, MEMORY_OBJECT_NULL, 0, FALSE, VM_PROT_READ | VM_PROT_WRITE,VM_PROT_READ | VM_PROT_WRITE,VM_INHERIT_DEFAULT);
+    mach_vm_map(gTask, &addr, PAGE_SIZE, 0, VM_FLAGS_ANYWHERE, MEMORY_OBJECT_NULL, 0, FALSE, VM_PROT_READ | VM_PROT_WRITE,VM_PROT_READ | VM_PROT_WRITE,VM_INHERIT_DEFAULT);
     printf("[*] Mapped memory at 0x%llx, size is %d bytes\n", (uint64_t)addr, PAGE_SIZE);
 
     return addr;
 }
 
-void virtRead(vm_address_t addr) {
-    vm_size_t read_size = PAGE_SIZE;
-    vm_offset_t data;
-    mach_msg_type_number_t bytes;
+void virtRead(mach_vm_address_t addr) {
+    mach_vm_size_t read_size = PAGE_SIZE;
 
-    vm_read(gTask, addr, read_size, &data, &bytes);
+    // Cannot use mach_vm_offset_t since for whatever reason mach_vm_read still uses vm_offset_t (unsigned long)
+    vm_offset_t data;
+
+    mach_msg_type_number_t bytes;
+    mach_vm_read(gTask, addr, read_size, &data, &bytes);
     
     printf("[*] Read %d bytes from address 0x%llx\n", bytes, (uint64_t)addr);
 }
 
-void virtWrite(vm_address_t addr, uint64_t value) {
+void virtWrite(mach_vm_address_t addr, uint64_t value) {
     mach_msg_type_number_t bytes = sizeof(value);
 
     // The vm_write API expects a pointer to the data being written, not the raw value; hence, 
     // we pass (pointer_t)&value to point to the address where the value is stored
     // otherwise we will get a invalid memory error
-    vm_write(gTask, addr, (pointer_t)&value, bytes);
+    mach_vm_write(gTask, addr, (pointer_t)&value, bytes);
     printf("[*] Wrote 0x%llx to 0x%llx\n", value, (uint64_t)addr);
 }
 
 int main() {
-    vm_address_t addr = task_map();
+    mach_vm_address_t addr = task_map();
 
     if(addr != 0)  {
         virtRead(addr);
